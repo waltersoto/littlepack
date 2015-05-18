8139aa718433092cd81f1c03e4c1305a849aff9e
Walter M. Soto <wmsoto@hotmail.com>
2015-05-18 09:30:03 -0400

    Version

--- LittlePack/LittlePack/Properties/AssemblyInfo.cs
+++ LittlePack/LittlePack/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿using System.Reflection; 
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -8,7 +7,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("LittlePack")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Walter M. Soto Reyes")]
 [assembly: AssemblyProduct("LittlePack")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
